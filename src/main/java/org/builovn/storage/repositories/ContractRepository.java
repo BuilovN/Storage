a5f59a5f0e67114d82f4cc37b26e2cf3617db1e0
Nikita <builovn@yandex.ru>
2020-11-06 17:43:39 +0300

    Repository class modifyed

--- src/main/java/org/builovn/storage/repositories/ContractRepository.java
+++ src/main/java/org/builovn/storage/repositories/ContractRepository.java
@@ -27,37 +27,83 @@ public class ContractRepository implements IRepository<Contract> {
         elements = newArray;
     }
 
+    private void checkIndex(int index){
+        if (index < 0 || index >= size){
+            throw new IndexOutOfBoundsException("Incorrect index " + index + " for length " + this.size);
+        }
+    }
+
     @Override
     public Contract get(int index){
-        if (index < 0 || index >= size){
-            throw new IndexOutOfBoundsException("Incorrect index " + index + "for length " + this.size);
-        }
-        else {
-            return elements[index];
-        }
+        checkIndex(index);
+        return elements[index];
+    }
+
+    public Contract getById(int id) {
+        return findById(id);
     }
 
     @Override
     public void add(Contract contract){
-
+        try{
+            elements[size] = contract;
+        } catch (IndexOutOfBoundsException e){
+            increaseCapacity();
+            elements[size] = contract;
+        }
+        size++;
     }
 
     @Override
     public Contract set(int index, Contract element) {
-        return null;
+        checkIndex(index);
+        Contract oldElement = elements[index];
+        elements[index] = element;
+        return oldElement;
     }
 
     @Override
     public Contract remove(int index) {
-        return null;
+        checkIndex(index);
+        Contract removableContract = elements[index];
+        for(int i = index; i < size - 1; i++){
+            elements[i] = elements[i + 1];
+        }
+        elements[size] = null;
+        size--;
+        return removableContract;
+    }
+
+    public Contract removeById(int id){
+        int indexOfRemovable = findIndexById(id);
+        if(indexOfRemovable != -1){
+            Contract contractToRemove = elements[indexOfRemovable];
+            remove(indexOfRemovable);
+            return contractToRemove;
+        } else {
+            return null;
+        }
     }
 
     public int getSize() {
         return size;
     }
 
-    public Contract getById(int id) {
+    private Contract findById(int id){
+        for (Contract element : elements) {
+            if (id == element.getId()) {
+                return element;
+            }
+        }
         return null;
     }
 
+    private int findIndexById(int id){
+        for (int i = 0; i < size; i++){
+            if(id == elements[i].getId()){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
