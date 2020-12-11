7e637f452766ed0557be9aeb0186800b7986b979
Nikita <builovn@yandex.ru>
2020-12-11 15:35:44 +0300

    IRepository method find added

--- src/main/java/org/builovn/storage/repositories/IRepository.java
+++ src/main/java/org/builovn/storage/repositories/IRepository.java
@@ -1,9 +1,13 @@
 package org.builovn.storage.repositories;
 
+import java.util.Optional;
+import java.util.function.Predicate;
+
 public interface IRepository<T> {
     void add(T element);
     T set(int index, T element);
     T remove(int index);
     T get(int index);
+    Optional<T> find(Predicate<T> predicate);
     int getSize();
 }
